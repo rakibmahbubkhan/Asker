1c44b0b4104f49b3ef6300276c20eaccf830cc7b
rakibmahbubkhan <rak.mbk@gmail.com>
2021-04-17 17:25:33 +0600

    showing questions

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuestionsController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('questions',QuestionsController::class);
