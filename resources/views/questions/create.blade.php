3a655b86c3ed1e7f54d1ad0a92a8fff340c312a5
rakibmahbubkhan <rak.mbk@gmail.com>
2021-04-18 00:45:54 +0600

    question form

---
+++ resources/views/questions/create.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h2>Ask Questions</h2>
+                        <div class="ml-auto">
+                            <a href="{{ route( 'questions.index') }}" class="btn btn-outline-secondary">back to all questions</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                   <form action="{{ route('questions.store') }}" method="POST">
+                       @csrf
+                       <div class="form-group">
+                           <label for="question-title">Title</label>
+                           <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
+                           @if($errors->has('title'))
+                            <div class="invalid-feedback">
+                                <strong>{{ $errors->first('title') }}</strong>
+                            </div>
+                           @endif
+                       </div>
+                       <div class="form-group">
+                           <label for="question-body">Description</label>
+                           <textarea name="body" id="question-body" cols="30" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"></textarea>
+                           @if($errors->has('body'))
+                            <div class="invalid-feedback">
+                                <strong>{{ $errors->first('body') }}</strong>
+                            </div>
+                           @endif
+                       </div>
+                       <div class="form-group">
+                           <button type="submit" class="btn btn-outline-primary btn-lg">Ask this question</button>
+                       </div>
+                   </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
