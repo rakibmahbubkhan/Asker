25f6e62333f53bdb27776355ccc04b40b4ee9988
rakibmahbubkhan <rak.mbk@gmail.com>
2021-06-05 11:02:31 +0600

    initial

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,9 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 
-        if (env('APP_ENV') !== 'local') {
+        if (config('APP_ENV') !== 'local') {
             URL::forceScheme('https');
-    }
+        }
 
         Paginator::useBootstrap();
     }
--- storage/framework/views/4f1cf5c9f03425d7c0b8bea27e8bb44081d2795a.php
+++ storage/framework/views/4f1cf5c9f03425d7c0b8bea27e8bb44081d2795a.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="<?php echo e(secure_asset('css/app.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/fontawesome.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" />
 
