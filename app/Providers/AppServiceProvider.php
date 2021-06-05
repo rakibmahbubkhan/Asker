f3d1862e36f2c35d3c74fd5bad968b64422be3fe
rakibmahbubkhan <rak.mbk@gmail.com>
2021-06-05 10:44:55 +0600

    initial

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        
+        if (env('APP_ENV') === 'production') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
+        
     }
 
     /**
@@ -24,10 +28,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
-        if (env('APP_ENV') === 'production') {
-            $this->app['request']->server->set('HTTPS', true);
-    }
 
         Paginator::useBootstrap();
     }
