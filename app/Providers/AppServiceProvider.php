59c13f707f6e09cd8d23aee75230c041eb99c180
rakibmahbubkhan <rak.mbk@gmail.com>
2021-06-05 12:02:36 +0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
 
         if (env('APP_ENV') !== 'local') {
-            URL::forceScheme('https');
+            URL::forceScheme('http');
         }
 
         Paginator::useBootstrap();
