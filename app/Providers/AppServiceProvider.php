a8383b47f196da4a5a78baafef898699e02e8f7b
rakibmahbubkhan <rak.mbk@gmail.com>
2021-06-05 10:51:41 +0600

    initial

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,11 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        
-        if (env('APP_ENV') === 'production') {
-            $this->app['request']->server->set('HTTPS', true);
-        }
-        
+        //
     }
 
     /**
@@ -28,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        if (env(key: 'APP_ENV') !== 'local') {
+            URL::forceScheme( scheme: 'https' );
+    }
 
         Paginator::useBootstrap();
     }
