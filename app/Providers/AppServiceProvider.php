50cf4672309cff6437bdbcc6b0269b23e28bf828
Rakib Mahbub Khan <52800057+rakibmahbubkhan@users.noreply.github.com>
2021-06-05 10:32:05 +0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 
-        if (env('APP_ENV') === 'production') {
+        if (config('APP_ENV') === 'production') {
             URL::forceSchema('https');
         }
 
